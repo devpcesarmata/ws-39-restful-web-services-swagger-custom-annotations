829b8baba94736de735e6e63729c2102eccca3f1
César <dev.pcesarmata@gmail.com>
2020-08-27 11:25:39 -0500

    init commit.

---
+++ src/main/java/ws39restfulwebservicesswaggercustomannotations/ws39restfulwebservicesswaggercustomannotations/Ws39RestfulWebServicesSwaggerCustomAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package ws39restfulwebservicesswaggercustomannotations.ws39restfulwebservicesswaggercustomannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws39RestfulWebServicesSwaggerCustomAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws39RestfulWebServicesSwaggerCustomAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws39restfulwebservicesswaggercustomannotations/ws39restfulwebservicesswaggercustomannotations/Ws39RestfulWebServicesSwaggerCustomAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ws39restfulwebservicesswaggercustomannotations.ws39restfulwebservicesswaggercustomannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws39RestfulWebServicesSwaggerCustomAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
